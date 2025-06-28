bdb8086cfa88aa4cb742b165c30b86d8061ace15
yachela <laura.yachelini@davinci.edu.ar>
2025-06-28 08:17:40 -0300

    Agrego datos a la BD cuando se instancia

--- app/src/main/java/com/example/turna/database/AppDatabase.java
+++ app/src/main/java/com/example/turna/database/AppDatabase.java
@@ -3,6 +3,8 @@ package com.example.turna.database;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import android.content.Context;
 
 import com.example.turna.model.Turno;
@@ -23,7 +25,18 @@ public abstract class AppDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, "turno_database")
-                            .fallbackToDestructiveMigration() // Para desarrollo
+                            .fallbackToDestructiveMigration().addCallback(new RoomDatabase.Callback() {
+                                @Override
+                                public void onCreate(@androidx.annotation.NonNull SupportSQLiteDatabase db) {
+                                    super.onCreate(db);
+                                    new Thread(() -> {
+                                        AppDatabase database = getInstance(context);
+                                        TurnoDao turnoDao = database.turnoDao();
+                                        turnoDao.insert(new Turno("Turno 1", "Consulta", "2025-07-01", "09:00", "Dr. Prueba"));
+                                        turnoDao.insert(new Turno("Turno 2", "Control", "2025-07-02", "11:00", "Lic. Ejemplo"));
+                                    }).start();
+                                }
+                            })
                             .build();
                 }
             }
